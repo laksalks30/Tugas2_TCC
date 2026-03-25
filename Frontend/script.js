1fbe77a9e463eb731de4c5a0618aee09e99cbca3
BliAli <aliaddamawi@gmail.com>
2026-03-25 19:36:07 +0700

    Update: IP VM Ternew (yang lama kehapus egiluy

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://35.225.67.56:3000/api/v1/notes";
+const API_BASE_URL = "http://34.171.237.227:3000/api/v1/notes";
 
 const noteForm = document.getElementById("noteForm");
 const noteIdInput = document.getElementById("noteId");
