b4cf9871c1a7e07f09c346f07ef5f529c368b8c3
laksalks30 <laksalks30@users.noreply.github.com>
2026-05-16 00:46:12 +0700

    add: app.yaml untuk App Engine

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://136.115.136.182:3000/api/v1/notes";
+const API_BASE_URL = "https://notes-backend-235-325409493725.us-central1.run.app/api/v1/notes";
 
 const noteForm = document.getElementById("noteForm");
 const noteIdInput = document.getElementById("noteId");
